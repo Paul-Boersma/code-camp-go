a025b089d003057af1403feb8f1aeb951bc2569c
Paul Boersma <pcboersma91@gmail.com>
2022-04-09 17:41:14 +0200

    set up a file server, use goilla/mux router

--- simple-web-server/go.mod
+++ simple-web-server/go.mod
@@ -1,3 +1,5 @@
 module github.com/Paul-Boersma/code-camp-go/simple-web-server
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- simple-web-server/main.go
+++ simple-web-server/main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world")
-	})
-	http.ListenAndServe(":8080", nil)
+	// File Servers provide a central location to store and share files for a network (static assets).
+	// Protocols: SBM (Server Message Block), NFS (Network File Server), FTP (File Transfer Protocol), SFTP (Secure FTP)
+	// A database is no File Server, as databases only deal with structured data accessed by a query.
+	// Indicate the directory with files to be served
+	// - NAS (Network attached storage) is dedicated file server hardware
+	// - DMS (Document Management System) is a file server dedicated to storing documents (Word, PDF, but not blob data or videos)
+	// If present, http.FileServer serves index.html by default, otherwise it shows the directory.
+	// The FileServer is used to serve Front End files (HTML, CSS, JavaScript) instead of plain text when handling (dynamic) requests.
+	fileServer := http.FileServer(http.Dir("static/"))
 
+	// To access the root directory of the file server, provide the root URL.
+	// With the go std http package, to access any file or directory within the root folder, provide the root URL: '/folder/somefile.html
+	// With the gorilla mux package, to acces a file or directory provide the full path from the current working directory, regardless of the path requested
+	r.Handle("/", fileServer)
+	// ! Required when using Mux
+	r.HandleFunc("/folder/", handleFolder)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func handleFolder(rw http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("./static/folder/index.html"))
+	tmpl.Execute(rw, nil)
 }
